d23e5928d184654e3218cd3ce4c0ce82d8687561
Kazim Muhammad <kazmodeston1@gmail.com>
2021-06-16 09:39:44 +0100

    created iceAndFireApi controller for an external Api

---
+++ app/Http/Controllers/ExternalApi/IceAndFireApisController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ExternalApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IceAndFireApisController extends Controller
+{
+    //
+}
