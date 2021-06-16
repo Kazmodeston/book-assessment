6980d804fe12d512d48e820e3721dfa36bf566bc
Kazim Muhammad <kazmodeston1@gmail.com>
2021-06-16 12:55:46 +0100

    created route for the external api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// App\Http\Controllers\ExternalApi\IceAndFireApisController::class;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/external-books/{nameOfABook}', [App\Http\Controllers\ExternalApi\IceAndFireApisController::class, "index"]);
