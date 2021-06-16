296f49ee0df923ba1390d4e1cafa0ab9af5351f1
Kazim Muhammad <kazmodeston1@gmail.com>
2021-06-16 19:11:56 +0100

    added api Resource route to capture all CRUD endpoints in the BooksController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+
+
+
+Route::prefix("v1")->group(function(){
+    Route::apiResource("/books", App\Http\Controllers\BooksController::class);
 });
